289821759121ebaaaf093fb17a187bb49124e9cf
HoangVu-Nguyen <hoangvufit2004@gmail.com>
2026-04-07 14:49:28 +0700

    feature(forgot):update component forgot-password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
 import { Active } from './features/auth/active/active';
+import { Forgot } from './features/auth/forgot/forgot';
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: 'active', component: Active },
+  { path: 'forgot', component: Forgot },
   { path: '**', redirectTo: 'login' }
 ];
