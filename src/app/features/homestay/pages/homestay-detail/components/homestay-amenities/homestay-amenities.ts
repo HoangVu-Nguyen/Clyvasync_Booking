d1450560fb8dfec7639bb1550d626cdf3b1e8614
HoangVu-Nguyen <hoangvufit2004@gmail.com>
2026-05-05 01:36:52 +0700

    feature(detail):create api get homestay detail by id

--- src/app/features/homestay/pages/homestay-detail/components/homestay-amenities/homestay-amenities.ts
+++ src/app/features/homestay/pages/homestay-detail/components/homestay-amenities/homestay-amenities.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, OnInit } from '@angular/core';
+import { HomestayService } from '../../../../../../core/services/homestay/homestay.service';
 
 @Component({
   selector: 'app-homestay-amenities',
@@ -6,4 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './homestay-amenities.html',
   styleUrl: './homestay-amenities.css',
 })
-export class HomestayAmenities {}
+export class HomestayAmenities implements OnInit {
+  constructor(private homestayService: HomestayService) {}
+  ngOnInit(): void {
+    console.log(this.amenities())
+  }
+ 
+
+    // Lấy dữ liệu trực tiếp từ Service (Reactive)
+    // Nếu dùng Signal trong HTML: {{ homestayService.currentHomestay()?.name }}
+    get homestay() {
+        return this.homestayService.currentHomestay();
+    }
+    amenities = computed(() => this.homestayService.currentHomestay() || []);
+
+
+    
+}
--- src/app/features/homestay/pages/homestay-detail/homestay-detail.ts
+++ src/app/features/homestay/pages/homestay-detail/homestay-detail.ts
@@ -5,10 +5,21 @@ import { HomestayAmenities } from './components/homestay-amenities/homestay-amen
 import { HomestayLocation } from './components/homestay-location/homestay-location';
 import { HomestayReviews } from './components/homestay-reviews/homestay-reviews';
 import { HomestayBookingWidget } from './components/homestay-booking-widget/homestay-booking-widget';
+import { HomestayService } from '../../../../core/services/homestay/homestay.service';
 @Component({
   selector: 'app-homestay-detail',
   imports: [HomestayHeader, HomestayGallery, HomestayAmenities,HomestayLocation, HomestayReviews, HomestayBookingWidget],
   templateUrl: './homestay-detail.html',
   styleUrl: './homestay-detail.css',
 })
-export class HomestayDetail {}
+export class HomestayDetail {
+  constructor(public homestayService: HomestayService) { }
+  
+  ngOnInit() {
+    const homestayId = 2; 
+    this.homestayService.getHomestayById(homestayId).subscribe(response => {
+      console.log('Chi tiết homestay:', response.data);
+    });
+  }
+
+}
