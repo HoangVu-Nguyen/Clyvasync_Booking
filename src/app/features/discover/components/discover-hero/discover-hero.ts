6cc51c2b34bcdd12b2ba027d5bd004ddb8308ba2
HoangVu-Nguyen <hoangvufit2004@gmail.com>
2026-05-04 13:37:47 +0700

    feat(ui): replace hero image with background video for better UX

--- src/app/features/discover/components/discover-hero/discover-hero.ts
+++ src/app/features/discover/components/discover-hero/discover-hero.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-discover-hero',
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './discover-hero.html',
   styleUrl: './discover-hero.css',
 })
-export class DiscoverHero {}
+export class DiscoverHero {
+  @ViewChild('videoRef') video!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit() {
+    const vid = this.video.nativeElement;
+    vid.muted = true;
+    vid.play().catch(err => {
+      console.log('Autoplay bị chặn:', err);
+    });
+  }
+}
